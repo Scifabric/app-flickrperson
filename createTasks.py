cd02298b26b6ed7673883fe23b83eb25ed44f492
Martin Keegan <martin@no.ucant.org>
2013-02-19 01:53:44 +0000

    split out image retrieval to separate file to make it easier to customise

--- createTasks.py
+++ createTasks.py
@@ -16,52 +16,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with PyBOSSA.  If not, see <http://www.gnu.org/licenses/>.
 
-import urllib
-import urllib2
 import json
-import re
-import string
 from optparse import OptionParser
 import pbclient
-
-def get_flickr_photos(size="big"):
-    """
-    Gets public photos from Flickr feeds
-    :arg string size: Size of the image from Flickr feed.
-    :returns: A list of photos.
-    :rtype: list
-    """
-    # Get the ID of the photos and load it in the output var
-    print('Contacting Flickr for photos')
-    url = "http://api.flickr.com/services/feeds/photos_public.gne"
-    values = {
-        'nojsoncallback': 1,
-        'format': "json",
-        #'ids':'25053835@N03'
-        }
-    query = url + "?" + urllib.urlencode(values)
-    urlobj = urllib2.urlopen(query)
-    data = urlobj.read()
-    urlobj.close()
-    # The returned JSON object by Flickr is not correctly escaped,
-    # so we have to fix it see
-    # http://goo.gl/A9VNo
-    regex = re.compile(r'\\(?![/u"])')
-    fixed = regex.sub(r"\\\\", data)
-    output = json.loads(fixed)
-    print('Data retrieved from Flickr')
-
-    # For each photo ID create its direct URL according to its size:
-    # big, medium, small (or thumbnail) + Flickr page hosting the photo
-    photos = []
-    for idx, photo in enumerate(output['items']):
-        print 'Retrieved photo: %s' % idx
-        imgUrl_m = photo["media"]["m"]
-        imgUrl_b = string.replace(photo["media"]["m"], "_m.jpg", "_b.jpg")
-        photos.append({'link': photo["link"], 'url_m':  imgUrl_m,
-                       'url_b': imgUrl_b})
-    return photos
-
+from get_images import get_flickr_photos
 
 if __name__ == "__main__":
     # Arguments for the application
